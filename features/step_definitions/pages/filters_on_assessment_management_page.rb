dffa2e26c5eea1491080b1eccc32289474050622
swapna gopu <swapna.gopu@wtg.co.uk>
2016-03-07 15:33:27 +0000

    auto script failures

--- features/step_definitions/pages/filters_on_assessment_management_page.rb
+++ features/step_definitions/pages/filters_on_assessment_management_page.rb
@@ -1,6 +1,7 @@
 class FiltersOnAssessmentManagementPage < SitePrism::Page
 
   elements :status_values, :xpath,  "html/body/div[1]/div[2]/div[2]/div/div/ul/li/div/label"
+  elements :assessment_status, ".assessment-status"
 
   def verify_status_filter_visibility(new_table)
     staus =[], actual_status_values = []
@@ -31,44 +32,36 @@ class FiltersOnAssessmentManagementPage < SitePrism::Page
     check('assessmentStatusChk0')
   end
 
-  def verify_reject_filter_status
-    @trainers.trainer_login_page.log_in("Assessor")
-    @trainers.ngu_search_assessment_id_page.request_assessment
-    @trainers.trainer_login_page.log_in("Compliance Manager")
-    @trainers.ngu_search_assessment_id_page.verify_requested_assessmemt_id_in_DB
+  def reject_assessment
+
     fill_in("txt-assessment-id",:with=>$requested_status)
     click_button("Search")
     click_button("Reject")
     fill_in("cancellationNotes", :with=>"Notes for Rejection")
     click_button('Yes')
-    find("#single-button").click
-    check('assessmentStatusChk2')
   end
 
-  def verify_cancelled_filter_status
-    @trainers.trainer_login_page.log_in("Assessor")
-    @trainers.ngu_search_assessment_id_page.book_assessment
-    @trainers.trainer_login_page.log_in("Compliance Manager")
-    @trainers.ngu_search_assessment_id_page.verify_booked_assessmemt_id_in_DB
+  def cancelled_assessment
     fill_in("txt-assessment-id",:with=>$booked_status)
     click_button("Search")
     click_button("Cancel")
     fill_in("cancellationNotes", :with=>"Notes for cancellation")
     click_button('Yes')
-    find("#single-button").click
-    check('assessmentStatusChk3')
+
   end
 
-  def verify_completed_filter_status
-    @trainers.trainer_login_page.log_in("Assessor")
-    @trainers.ngu_search_assessment_id_page.book_assessment
-    @trainers.trainer_login_page.log_in("Compliance Manager")
-    @trainers.ngu_search_assessment_id_page.verify_booked_assessmemt_id_in_DB
+  def completed_assessment
     fill_in("txt-assessment-id",:with=>$booked_status)
     click_button("Search")
-    @trainers.assessment_form_for_marking_outcome_page.select_outcome_against_trainer
-    click_button('Mark Complete')
-    find("#single-button").click
-    check('assessmentStatusChk4')
+
+  end
+
+  def verify_combination_status(status1,status2)
+    status_array= [status1,status2]
+    assessment_status.each do |status|
+      actual_status = status.text
+
+      puts expect(status_array).to include(actual_status)
+    end
   end
 end
