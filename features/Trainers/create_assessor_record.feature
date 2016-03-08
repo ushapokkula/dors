d159ce89663b262d1a352eae9e0962f1ec9a5004
Anita.Hegde <Anita.Hegde@wtg.co.uk>
2016-03-08 10:26:43 +0000

    Create assessor

--- features/Trainers/create_assessor_record.feature
+++ features/Trainers/create_assessor_record.feature
@@ -31,7 +31,7 @@ Feature: DR-171
     Then I see the following fields as "<Mandatory>" with "<Error Messages>"
 
     Examples:
-      | Mandatory            | Error Messages                   |
+           | Mandatory            | Error Messages                   |
       | Username             | Please provide a username.       |
       | First Name           | Please provide a first name.     |
       | Last Name            | Please provide a last name.      |
@@ -56,3 +56,37 @@ Feature: DR-171
   Scenario: Verify the Cancel and Create Assessor buttons
     And I see that the page includes "Create Assessor" and "Cancel" buttons
 
+
+  @create_assessor5
+  Scenario: Verify Cancel button on create assessor page is working
+    Then the system will load the page where I can create a new assessor record
+    When I click Cancel button
+    Then then unsaved changes will be lost and I will be redirected to my homepage (Assessments Management)
+
+  @create_assessor6
+  Scenario Outline: Verify all validation messages on entering invalid inputs to create assessor form
+    Then the system will load the page where I can create a new assessor record
+    Then I enter field1 "<username>"
+    Then I enter field2 "<Assessor Number>"
+    Then I enter field3 "<First Name>"
+    Then I enter field4 "<Last Name>"
+    Then I enter field5 "<Primary Phone Number>"
+    Then I enter field6 "<Secondary Phone Number>"
+    Then I enter field7 "<Email>"
+    Then I enter field8 "<Address>"
+    Then I enter field9 "<Town>"
+    Then I enter field10 "<Postcode>"
+    Then I enter field11 "<Force Areas>"
+    Then I click on create assessor button
+    Then I see validation messages for "<username>","<Assessor Number>","<First Name>","<Last Name>","<Primary Phone Number>","<Secondary Phone Number>","<Email>","<Address>","<Town>","<Postcode>","<Force Areas>"
+
+  Examples:
+  |username|Assessor Number|First Name|Last Name|Primary Phone Number|Secondary Phone Number|Email|Address|Town|Postcode|Force Areas|
+  |          |11111111| WTGTest| Web Technologies| 07987876789|07876545654|test.wtg@wtg.co.uk|76 Hammersmith road|London|W148UD|BRITISH TRANSPORT POLICE|
+  #| TestWTG  |11111111|    | Web Technologies| 07987876789|07876545654|test.wtg@wtg.co.uk|76 Hammersmith road|London|W148UD|BRITISH TRANSPORT POLICE|
+  #| TestWTG  |11111111| WTG|                 | 07987876789|07876545654|test.wtg@wtg.co.uk|76 Hammersmith road|London|W148UD|BRITISH TRANSPORT POLICE|
+  #| TestWTG  |11111111| WTG| Web Technologies| |07876545654|test.wtg@wtg.co.uk|76 Hammersmith road|London|W148UD|BRITISH TRANSPORT POLICE|
+  #| TestWTG  |11111111| WTG| Web Technologies| 07987876789|07876545654|                  |76 Hammersmith road|London|W148UD|BRITISH TRANSPORT POLICE|
+ # | TestWTG  |11111111| WTG| Web Technologies| 07987876789|07876545654|test.wtg@wtg.co.uk|               |London|W148UD|BRITISH TRANSPORT POLICE|
+  #| TestWTG  |11111111| WTG| Web Technologies| 07987876789|07876545654|test.wtg@wtg.co.uk|76 Hammersmith road| |W148UD|BRITISH TRANSPORT POLICE|
+  #| TestWTG  |11111111| WTG| Web Technologies| 07987876789|07876545654|test.wtg@wtg.co.uk|76 Hammersmith road|London|   |BRITISH TRANSPORT POLICE|
--- features/step_definitions/pages/create_assessor_record_page.rb
+++ features/step_definitions/pages/create_assessor_record_page.rb
@@ -65,4 +65,262 @@ class CreateAssessorRecordPage < SitePrism::Page
      expect(new_table.map { |x| x['Assessor Input Fields'] }).to match_array(assessor_fields)
   end
 
+  def isAssessorCreatePage()
+    should have_content('Assessors management')
+    should have_content('Create New Assessor')
+    find_button('Create Assessor')
+    puts "Yes I am on Create assessor page"
+  end
+
+  def clickOnCancelButton()
+    click_button('Cancel')
+  end
+
+  def isYourHomePage()
+    should have_content('Assessments')
+  end
+
+  def validateUsername(username)
+
+    result = false
+    usernameLength = username.length
+    result = has_content?("Sorry, the username is already taken. Please try a different username.")
+     puts "#{username}"
+      # if (username.eql? nil)
+
+    if (username.empty?)
+      should have_content("Please provide a username.")
+
+
+         elsif((usernameLength<=3)&&(usernameLength>=1))
+
+           should have_content('Sorry, the username must be at least 4 characters long.')
+
+                elsif(usernameLength>=71)
+
+                  should have_content('Sorry, the username must not be more than 70 characters long.')
+
+                  elsif(result.eql? true)
+
+                       # TO DO - Check this username already there in DB to make sure validation message is correct
+                           puts "The account already exists"
+      end
+  end
+
+  def validateAssessorNumber(assessorNumber)
+
+    if (assessorNumber.empty?)
+
+      puts "Ok, Assessor number is optional"
+
+      else
+        assessorNumberLength = assessorNumber.length
+
+        if (assessorNumberLength>=9)
+
+          should have_content('Something wrong with the Assessor number input- May be length is more than allowed!!')
+
+        end
+
+    end
+
+  end
+
+  def validatefirstName(firstName)
+
+    firstNameLength = firstName.length
+
+    if (firstName.empty?)
+
+      find('Please provide a first name.')
+
+      elsif(firstNameLength<=3)
+      #TO-DO check with the length validation as it is not yet implemented
+        should have_content('Sorry, the first name must be at least 4 characters long.')
+
+      #TO-DO check with the length validation as it is not yet implemented
+      elsif(firstNameLength>=21)
+        should have_content('Sorry, the first name must not be more than 20 characters long.')
+    end
+
+  end
+
+def validateLastName(lastName)
+
+  lastNameLength = lastName.length
+
+  if (lastName.empty?)
+    find('Please provide a last name.')
+  elsif(lastNameLength<=3)
+    #TO-DO check with the length validation as it is not yet implemented
+    should have_content('Sorry, the last name must be at least 4 characters long.')
+
+    #TO-DO check with the length validation as it is not yet implemented
+  elsif(lastNameLength>=21)
+    should have_content('Sorry, the last name must not be more than 20 characters long.')
+  end
+
 end
+
+  def validatePrimaryPhoneNumber(primaryPhoneNumber)
+
+    primaryPhoneNumberLength = primaryPhoneNumber.length
+
+       if (primaryPhoneNumber.empty?)
+
+      should have_content('Please provide a phone number.')
+
+    elsif(primaryPhoneNumberLength<=9)
+
+      should have_content('Sorry, the phone number must be at least 10 digits long.')
+
+     elsif (primaryPhoneNumber[0]!= '0')
+
+       should have_content('Sorry, the phone number must start with zero')
+
+       elsif((primaryPhoneNumber.to_i).is_a?(Numeric).eql? false)
+            should have_content('Sorry, only numbers are accepted.')
+    end
+
+  end
+
+  def validatesecondaryPhoneNumber(secondaryPhoneNumber)
+
+    secondaryPhoneNumberLength = secondaryPhoneNumber.length
+
+        if (secondaryPhoneNumber.empty?)
+
+      should have_content('Please provide a phone number.')
+
+    elsif(secondaryPhoneNumberLength<=9)
+
+      should have_content('Sorry, the phone number must be at least 10 digits long.')
+
+    elsif (secondaryPhoneNumber[0]!= '0')
+
+      should have_content('Sorry, the phone number must start with zero')
+
+    elsif((secondaryPhoneNumber.to_i).is_a?(Numeric).eql? false)
+      should have_content('Sorry, only numbers are accepted.')
+    end
+
+  end
+
+def validateEmail(email)
+
+   x= email.match(/[a-zA-Z0-9._%]@(?:[a-zA-Z0-9]\.)[a-zA-Z]{2,4}/)
+
+  p x
+
+  end
+
+
+  def validateAddress(address)
+
+    if (address.empty?)
+
+      should have_content('Please provide an address.')
+
+  end
+
+  end
+
+  def validateTown(town)
+
+    if (town.empty?)
+
+      should have_content('Please provide a town.')
+
+  end
+
+  end
+
+
+  def validatePostcode(postcode)
+
+
+    if (postcode.empty?)
+
+      should have_content('Please provide a postcode.')
+
+    end
+
+
+  end
+
+  def selectForceAreas(forceareas)
+
+    fill_in('assessorForceAreas', :with=> forceareas)
+   # select(forceareas, :from => 'typeahead-512-9634-option-0')
+   # select('typeahead-512-9634-option-0', :from => 'assessorForceAreas')
+  end
+
+  def createAssessor()
+
+    click_button('Create Assessor')
+
+  end
+
+  def fillinUserName(username)
+
+    fill_in('assessorUsername', :with=> username)
+
+  end
+
+  def fillinassessorNumber(assessorNumber)
+
+    fill_in('assessorNumber', :with=> assessorNumber)
+
+  end
+
+  def fillinfirstName(firstName)
+
+    fill_in('assessorFirstName', :with=> firstName)
+
+  end
+
+  def fillinlastName(lastName)
+
+    fill_in('assessorLastName', :with=> lastName)
+
+  end
+
+  def fillinprimaryPhoneNumber(primaryPhoneNumber)
+
+    fill_in('assessorPhone', :with=> primaryPhoneNumber)
+
+  end
+
+  def fillinsecondaryPhoneNumber(secondaryPhoneNumber)
+
+    fill_in('assessorSecondaryPhone', :with=> secondaryPhoneNumber)
+
+  end
+
+  def fillinemail(email)
+
+    fill_in('assessorEmail', :with=> email)
+
+  end
+
+  def fillinaddress(address)
+
+    fill_in('assessorAddress', :with=> address)
+
+  end
+
+  def fillintown(town)
+
+    fill_in('assessorTown', :with=> town)
+
+  end
+
+  def fillinpostcode(postcode)
+
+    fill_in('assessorPostcode', :with=> postcode)
+
+  end
+
+
+
+  end
