6a2a10232efcfd1f77e5201e4c0e2b10f1fa374e
roopa.ramisetty <roopa.ramisetty@wtg.co.uk>
2016-05-06 16:29:02 +0100

    added step defs for DR-261

--- features/Trainers/Show_AssessmentID_On_Assessment_Requests.feature
+++ features/Trainers/Show_AssessmentID_On_Assessment_Requests.feature
@@ -7,33 +7,31 @@ Feature:
   Background:
     Given that I have licence.ndors.org.uk page opened
     When I login as an "Assessor3"
-    And I click on "REQUEST ASSESSMENT" page
+    And I click on "REQUEST ASSESSMENT"
     Then default view of the request assessments page is loaded
-    And I request assessments
+    And I request assessment
 
    Scenario: The summary view of assessment requests on My Assessments page should show the Assessment ID.
-    And I navigate to "MY ASSESSMENTS" page
-    When I select "Requested" status from Assessment Status dropdown
+    And I click "MY ASSESSMENTS"
+    When I select 'Requested' status from Assessment Status dropdown
     And I see requested assessments on My Assessments page
-    Then I should see the "Assessment ID" on summary view of request assessments
+    Then I should see the "Assessment ID" on summary view of assessment requests on My assessment page
 
 
    Scenario: The detailed view of an assessment request should show Assessment ID.
     And I navigate to "MY ASSESSMENTS" page
-    When I select "Requested" status from Assessment Status dropdown
+    When I select 'Requested' status from Assessment Status dropdown
     And I click on "View Details" button on My assessments page
-    #Then I should see "My assessment details" page
-    And I should see "Assessment ID" on detailed view of request assessment
+    Then I should see "My assessment details" page
+    And I should see "Assessment ID" on detailed view of assessment request
 
     Scenario: The summary view of assessment requests on Manage Assessments page should show the Assessment ID.
      Then I login as an "Compliance Manager"
       And I click "ASSESSMENT MANAGEMENT"
-      Then I will see list of all Assessments Requests with status requested
       And I should see "Assessment ID" on summary view of assessment requests on Manage Assessments page
 
      Scenario: The detailed view of an assessment request should show Assessment ID.
       Then I login as an "Compliance Manager"
       And I click "ASSESSMENT MANAGEMENT"
-      Then I will see list of all Assessments Requests with status requested
       And I click on "View Details" button on Assessment management page
       Then I should see "Assessment ID" on detailed view of an assessment request
--- features/step_definitions/Show_AssessmentID_On_Assessment_Requests.steps.rb
+++ features/step_definitions/Show_AssessmentID_On_Assessment_Requests.steps.rb
@@ -1,28 +1,69 @@
-Then (/^I click on "([^"]*)" page$/)do
+And(/^I click on "([^"]*)"$/)do|button_link|
   @trainers.ngu_search_assessment_id_page.delete_assessments_from_DB
-  click_button("REQUEST ASSESSMENT")
+  click_link_or_button(button_link)
   end
 
 Then(/^default view of the request assessments page is loaded$/)do
-  expect(page).to have_css("h1",text: 'Request Assessment')
-  expect(page).to have_css(".dors-well-other")
+  expect(page).to have_css("h1", text: 'Request Assessment')
+  expect(page).to have_css(".dors-table")
 end
 
+And (/^I request assessment$/)do
+  @trainers.date_filter_on_assessment_management_page.request_assessments_without_nearby_course
+  expect(page).to have_css(".alert.alert-success", text:'The assessment has been Requested')
+end
 
-Then (/^I select "Requested" status from Assessment Status dropdown$/)do
+Then (/^I select 'Requested' status from Assessment Status dropdown$/)do
+  expect(page).to have_css("h1", text: 'My assessments')
   page.find("#single-button").click
-  find("#assessmentStatusChk0").click
+  check('assessmentStatusChk0')
 end
 
 Then (/^I see requested assessments on My Assessments page$/)do
  expect(page).to have_css(".dors-well-other")
-  find(".dors-well-other").count.eql?("")
+ page.find_all('.dors-well-other').count.eql?("")
 end
 
-Then (/^I should see the "([^"]*)" on summary view of request assessments$/)do
+Then (/^I should see the "([^"]*)" on summary view of assessment requests on My assessment page$/)do|assesmentID|
   expect(page).to have_css(".dors-well-other")
   within(".dors-well-other") do
-    page.find(".assessment-id")
-    expect(page).to have_text("Assessment ID")
+    expect(page).to have_text(assesmentID)
+    expect(page).to have_css(".assessment-id", visible:true)
+  end
+end
+
+And (/^I click on "([^"]*)" button on My assessments page$/)do |view_details|
+  click_link_or_button(view_details)
+end
+
+Then (/^I should see "([^"]*)" page$/)do |my_assesment_page|
+  expect(page).to have_css("h1", text: my_assesment_page)
+end
+
+And (/^I should see "([^"]*)" on detailed view of assessment request$/)do |assessmentID|
+expect(page).to have_css(".dors-table")
+within(".dors-table") do
+  expect(page).to have_text(assessmentID)
+  expect(page).to have_css(".assessmentIdHeader", visible:true)
+  end
+end
+
+And (/^I should see "([^"]*)" on summary view of assessment requests on Manage Assessments page$/)do |assessmentID|
+  expect(page).to have_css(".dors-table")
+  within(".dors-table") do
+    expect(page).to have_text(assessmentID)
+    expect(page).to have_css(".text-md", visible:true)
+  end
+end
+
+And (/^I click on "([^"]*)" button on Assessment management page$/)do|view_details|
+  click_link_or_button(view_details)
+end
+
+Then (/^I should see "([^"]*)" on detailed view of an assessment request$/)do |assesmentID|
+  expect(page).to have_css(".dors-table")
+  within(".dors-table") do
+    expect(page).to have_text(assesmentID)
+    expect(page).to have_css(".assessmentIdHeader", visible:true)
   end
 end
