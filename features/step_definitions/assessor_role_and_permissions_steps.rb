111844b6c09cc2767df1807b5941a6b29b016391
Raja Sekhar <raja.sekhar@wtg.co.uk>
2016-05-27 16:14:54 +0100

    expect have_non_link

--- features/step_definitions/assessor_role_and_permissions_steps.rb
+++ features/step_definitions/assessor_role_and_permissions_steps.rb
@@ -11,7 +11,7 @@ And(/^I will be directed to My assessments page$/) do
 end
 
 And(/^The page will show the list of assessments with status Approved$/) do
-  click_link_or_button("MY ASSESSMENTS")
+  click_link_or_button("MY ASSESSMENTS") # TODO seperate step
   @trainers.assessor_role_and_permissions_page.verify_assessments_list_with_status
 end
 
@@ -30,6 +30,7 @@ And(/^I request assessments to be booked$/) do
 end
 
 Then(/^The page will show the list of assessments "([^"]*)" requested$/) do |user|
+  #TODO seperate steps
   click_link("MY ASSESSMENTS")
   expect(page).to have_css(".dors-well-other")
   find(:button, "View Details").click
@@ -38,8 +39,6 @@ Then(/^The page will show the list of assessments "([^"]*)" requested$/) do |use
 end
 
 Then(/^I see that I have no access to "([^"]*)","([^"]*)"$/) do |tab1, tab2|
-  sleep 3
-  page.has_no_link?(tab1, {}).should == true
-  page.has_no_link?(tab2, {}).should == true
-
+  expect(page).to have_no_link(tab1)
+  expect(page).to have_no_link(tab2)
 end
