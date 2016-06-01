cc8fa31a992613ce9e1c6cc0bb5fc8257f85cbda
roopa.ramisetty <roopa.ramisetty@wtg.co.uk>
2016-06-01 13:46:29 +0100

    updated DR-398 steps

--- features/step_definitions/pages/Date_Filter_On_Assessment_Management_Page.rb
+++ features/step_definitions/pages/Date_Filter_On_Assessment_Management_Page.rb
@@ -30,21 +30,14 @@ class DateFilterOnAssessmentManagementPage < SitePrism::Page
   click_link_or_button("Submit")
     end
 
-   def book_assessments_without_milage
-  click_link("REQUEST ASSESSMENT")
-  find(:button,'Pick a slot',match: :first).click if find(:button,'Pick a slot', match: :first)
-  first(:button,'Request Assessment').click if find(:button,'Request Assessment',match: :first)
-  find('.include-main-trainer-checkbox').click
-  click_link_or_button("Submit")
-  end
 
   def book_assessments_without_milage
     click_link("REQUEST ASSESSMENT")
     expect(page).to have_css("h1", text: 'Request Assessment')
     find(:button,'Pick a slot',match: :first).click if find(:button,'Pick a slot', match: :first)
    first(:button,'Request Assessment').click if find(:button,'Request Assessment',match: :first)
-   page.find_all(".include-main-trainer-checkbox", match: :first)
-   find('.include-main-trainer-checkbox', match: :first).click if find('.include-main-trainer-checkbox', match: :first)
+   page.find_all(".include-nearby-trainer-checkbox", match: :first)
+   find('.include-nearby-trainer-checkbox', match: :first).click if find('.include-nearby-trainer-checkbox', match: :first)
    click_link_or_button("Submit")
     within('.alert.alert-success.ng-binding') do
 
