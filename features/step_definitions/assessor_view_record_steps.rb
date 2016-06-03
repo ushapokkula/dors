97383ddd037c7ca7409de5a24d034b124f07686a
Swapna.Gopu <swapna.gopu@wtg.co.uk>
2016-06-03 16:49:32 +0100

    Assessor view record failing only in CI, added assertion

--- features/step_definitions/assessor_view_record_steps.rb
+++ features/step_definitions/assessor_view_record_steps.rb
@@ -1,14 +1,15 @@
-And(/^I will be shown my record with below read only "([^"]*)" fields$/)do |field_name|
-
-   if(field_name == "Force Areas")
-   page.should have_no_css("#selected-force-areas .close")                     #/*need to implement case statement*/
-   else
-   field_labeled(field_name, :disabled=> true)
+And(/^I will be shown my record with below read only "([^"]*)" fields$/) do |field_name|
+  expect(page).to have_css("h1", text: "My profile")
+  if (field_name == "Force Areas")
+    page.should have_no_css("#selected-force-areas .close") #/*need to implement case statement*/
+  else
+    field_labeled(field_name, :disabled => true)
   end
 end
 
-And(/^I will be shown my record with below editable "([^"]*)" fields$/)do |field_name|
-   field_labeled(field_name, :disabled=> false)
+And(/^I will be shown my record with below editable "([^"]*)" fields$/) do |field_name|
+  expect(page).to have_css("h1", text: "My profile")
+  field_labeled(field_name, :disabled => false)
 end
 
 
