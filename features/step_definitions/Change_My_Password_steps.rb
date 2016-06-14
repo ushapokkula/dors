ad56561775fbe4a6e39cc59a0770259adbfe3f59
roopa.ramisetty <roopa.ramisetty@wtg.co.uk>
2016-06-14 14:34:39 +0100

    DR-443 inprogress

--- features/step_definitions/Change_My_Password_steps.rb
+++ features/step_definitions/Change_My_Password_steps.rb
@@ -1,21 +1,24 @@
 Then (/^the system will show user 'Profile details' in expand section$/)do
-  expect(page.should have_css(".panel-open", visible:true))
+  expect(page.should have_css(".panel-group .panel:nth-child(2).panel-open", visible:true))
   expect(page.should have_button("Update", visible: true))
   expect(page.should have_button("Cancel", visible: true))
 end
 
 And (/^the 'Change password' section will be collapsed by default$/)do
-
+  expect(page.should_not have_css(".panel-group .panel:nth-child(1).panel-open", visible:false))
 end
 
 When (/^I request to expand the 'Change password' section$/)do
-
+  find("#lnk-toggle-change-password-form").click
 end
 
 Then (/^'Change password' section will be expanded$/)do
-
+  expect(page.should have_css(".panel-group .panel:nth-child(1).panel-open", visible:true))
+  expect(page.should have_css(".alert.alert-info", text: 'Please enter your current password and the new password you want to set on your account. Your new password must meet the password policy requirements.',visible: true))
+  expect(page.should have_css("#currentPassword", visible: true))
 end
 
 And (/^'Profile details' section will be collapsed$/)do
-
+  expect(page.should_not have_css(".panel-group .panel:nth-child(2).panel-open", visible:false))
+  expect(page.should_not have_button("Update", visible: true))
 end
