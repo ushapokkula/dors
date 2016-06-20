92033b0cf0bb6faaff28fdc850212bb6d7cdf665
Swapna.Gopu <swapna.gopu@wtg.co.uk>
2016-06-20 14:37:05 +0100

    added new scenarios for  feature DR-44

--- features/step_definitions/trainer_completes_creation_of_their_account_steps.rb
+++ features/step_definitions/trainer_completes_creation_of_their_account_steps.rb
@@ -1,3 +1,10 @@
 Then(/^I see the message "([^"]*)" after creating trainer$/) do |text|
   expect(page).to have_css(".toast-message", text: text)
 end
+
+
+And(/I will be shown password policy requirements to set the password$/)do
+puts expect(page).to have_css(".popover.ng-isolate-scope.fade.in.bottom", visible:true)
+puts expect(page).to have_css(".password-policy-requirement",visible:true)
+end
+
