6c10e36c8e8fd100ef8b84dc793436ede955dac9
Swapna.Gopu <swapna.gopu@wtg.co.uk>
2016-07-07 11:26:33 +0100

    added new scenarios to DR-904

--- features/Trainers/Trainer/update_licence_agreement.feature
+++ features/Trainers/Trainer/update_licence_agreement.feature
@@ -1,17 +1,18 @@
-@DR-90
+@DR-900
 
   Feature: Record User's identity and date-time stamp when licence agreement is set/updated by trainer or CCU
 
     @trainer_accept
     Scenario: Verify user's identity and date-time stamp when licence agreement is set to accepted by trainer
       Given that I have licence.ndors.org.uk page opened
-      And I login as an "Complaince Manager"
+      And I login as an "Compliance Manager"
       Given I have licence agreement as "Unspecified" for trainer Id "123987"
       And I logout
       And I login as an "Trainer Licence"
       When I click "Accept"
       And I will be redirected to "My Licences" page
       When I login as an "Complaince Manager"
+      And I click "TRAINERS"
       And search for trainer "123987"
       And I can see "Accepted" as Licence Agreement
       And I can see the last changed details with user fullname and updated date
--- features/step_definitions/update_licence_agreement_steps.rb
+++ features/step_definitions/update_licence_agreement_steps.rb
@@ -1,21 +1,22 @@
 Given(/^I have licence agreement as "([^"]*)" for trainer Id "([^"]*)"$/) do |agreement_status, trainer_id|
-  click("TRAINERS")
+  click_link("TRAINERS")
   fill_in('txt-trainer-name', :with => trainer_id).send_keys(:enter)
-
-
+  select(agreement_status, :from=>'licenseAgreementStatus')
+  click_button("Update Trainer")
+  expect(page).to have_css(".taost-message", text:'New trainer updated successfully')
 end
 
 
 And(/^I will be redirected to "([^"]*)" page$/) do |page|
-
+ expect(page).to have_css('h1',text: page)
 end
 
 And(/^search for trainer "([^"]*)"$/) do |arg|
-
+  fill_in('txt-trainer-name', :with => trainer_id).send_keys(:enter)
 end
 
-And(/^I can see "([^"]*)" as Licence Agreement$/) do |arg|
-
+And(/^I can see "([^"]*)" as Licence Agreement$/) do |agreement_status|
+  expect(page).to have_select('licenseAgreementStatus', :selected=> agreement_status)
 end
 
 And(/^I can see the last changed details with user fullname and updated date$/) do
