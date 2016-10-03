4345abce60fbe1bb03f364d945f383d565c67698
roopa.ramisetty <roopa.ramisetty@wtg.co.uk>
2016-10-03 11:09:06 +0100

    updated env file

--- features/support/env.rb
+++ features/support/env.rb
@@ -34,8 +34,8 @@ else
 end
 
 Capybara.configure do |config|
-  #config.app_host = "https://auto.trainer.dors.wtg.co.uk"
-  config.app_host = "https://test2.trainer.dors"
+  config.app_host = "https://auto.trainer.dors.wtg.co.uk"
+  #config.app_host = "https://test2.trainer.dors"
   config.default_max_wait_time = 25
 end
 
