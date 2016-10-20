1c286959d0124f787ff4f926b1c66d3da37abfe1
Swapna.Gopu <swapna.gopu@wtg.co.uk>
2016-10-20 14:51:27 +0100

    added new scenarios to DR-385

--- features/step_definitions/filters_on_my_assessment_steps.rb
+++ features/step_definitions/filters_on_my_assessment_steps.rb
@@ -1,8 +1,8 @@
-And(/^I am on the My Assessments page$/)do
+And(/^I am on the My Assessments page$/) do
   expect(page).to have_content("My assessments")
 end
 
-When(/^The default view of the page of My assessments is loaded$/)do
+When(/^The default view of the page of My assessments is loaded$/) do
   @trainers.ngu_search_assessment_id_page.delete_assessments_from_DB
   click_link("REQUEST ASSESSMENT")
   @trainers.ngu_search_assessment_id_page.book_assessment
@@ -16,7 +16,7 @@ Then(/^I can see the assessment status filter with these following options on My
   @trainers.filters_on_my_assessment_page.verify_status_filter_visibility_on_myassessments(new_table)
 end
 
-When(/^I select "([^"]*)" on My Assessments page$/)do |status_filter|
+When(/^I select "([^"]*)" on My Assessments page$/) do |status_filter|
   @trainers.ngu_search_assessment_id_page.delete_assessments_from_DB
   if (status_filter == "APPROVED")
 
@@ -94,7 +94,7 @@ When(/^I select "([^"]*)" on My Assessments page$/)do |status_filter|
   end
 end
 
-Then(/^I see that the "([^"]*)" option is selected by default on my assessments page$/)do|default_status|
+Then(/^I see that the "([^"]*)" option is selected by default on my assessments page$/) do |default_status|
   within(".dropdown-menu") do
     find("input[type='checkbox']:checked + label").text == default_status
     find("#assessmentStatusChk1").should be_checked
@@ -103,9 +103,9 @@ Then(/^I see that the "([^"]*)" option is selected by default on my assessments 
 end
 
 
-When(/^I select "([^"]*)" and "([^"]*)" on My Assessments page$/)do |status_filter1,status_filter2|
+When(/^I select "([^"]*)" and "([^"]*)" on My Assessments page$/) do |status_filter1, status_filter2|
   @trainers.ngu_search_assessment_id_page.delete_assessments_from_DB
-  if (status_filter1 == "REQUESTED" && status_filter2 == "APPROVED" )
+  if (status_filter1 == "REQUESTED" && status_filter2 == "APPROVED")
 
     @trainers.ngu_search_assessment_id_page.book_assessment
     expect(page).to have_selector(".alert.alert-success")
@@ -157,54 +157,61 @@ When(/^I select "([^"]*)" and "([^"]*)" on My Assessments page$/)do |status_filt
     check('assessmentStatusChk2')
     check('assessmentStatusChk3')
 
-  else (status_filter1 == "REJECTED" && status_filter2 == "COMPLETED")
+  else
+    (status_filter1 == "REJECTED" && status_filter2 == "COMPLETED")
 
-  @trainers.ngu_search_assessment_id_page.book_assessment
-  expect(page).to have_selector(".alert.alert-success")
-  @trainers.ngu_search_assessment_id_page.request_assessment
-  expect(page).to have_selector(".alert.alert-success")
-  click_link("Sign out")
-  @trainers.trainer_login_page.login_as("Compliance Manager")
-  @trainers.ngu_search_assessment_id_page.verify_requested_assessmemt_id_in_DB
-  @trainers.filters_on_my_assessment_page.reject_assessment_on_myassessments
-  @trainers.ngu_search_assessment_id_page.verify_booked_assessmemt_id_in_DB
-  @trainers.filters_on_assessment_management_page.completed_assessment
-  @trainers.assessment_form_for_marking_outcome_page.select_outcome_against_trainer
-  click_button('Mark Complete')
-  expect(page).to have_selector(".alert.alert-success")
-  click_link("Sign out")
-  @trainers.trainer_login_page.login_as("Assessor")
-  click_link("MY ASSESSMENTS")
-  find("#single-button").click
-  expect(page).to have_css("#single-button + .dropdown-menu")
-  check('assessmentStatusChk3')
-  check('assessmentStatusChk4')
+    @trainers.ngu_search_assessment_id_page.book_assessment
+    expect(page).to have_selector(".alert.alert-success")
+    @trainers.ngu_search_assessment_id_page.request_assessment
+    expect(page).to have_selector(".alert.alert-success")
+    click_link("Sign out")
+    @trainers.trainer_login_page.login_as("Compliance Manager")
+    @trainers.ngu_search_assessment_id_page.verify_requested_assessmemt_id_in_DB
+    @trainers.filters_on_my_assessment_page.reject_assessment_on_myassessments
+    @trainers.ngu_search_assessment_id_page.verify_booked_assessmemt_id_in_DB
+    @trainers.filters_on_assessment_management_page.completed_assessment
+    @trainers.assessment_form_for_marking_outcome_page.select_outcome_against_trainer
+    click_button('Mark Complete')
+    expect(page).to have_selector(".alert.alert-success")
+    click_link("Sign out")
+    @trainers.trainer_login_page.login_as("Assessor")
+    click_link("MY ASSESSMENTS")
+    find("#single-button").click
+    expect(page).to have_css("#single-button + .dropdown-menu")
+    check('assessmentStatusChk3')
+    check('assessmentStatusChk4')
 
   end
 
 end
 
 And(/^I can see "([^"]*)" section on My assessments page$/) do |section_header|
- expect(page).to have_css(".panel-title.panel-title-adj-btn strong", visible:true, text:section_header)
+  expect(page).to have_css(".panel-title.panel-title-adj-btn strong", visible: true, text: section_header)
 end
 
 And(/^I can see course filter under "([^"]*)" section with label "([^"]*)"$/) do |section_header, course_label|
-within(".panel.panel-default")do
-  expect(page).to have_css(".panel-title.panel-title-adj-btn strong", visible:true, text: section_header)
-  expect(page).to have_css(".col-md-3.has-feedback>label", visible:true, text: course_label)
-end
+  within(".panel.panel-default") do
+    expect(page).to have_css(".panel-title.panel-title-adj-btn strong", visible: true, text: section_header)
+    expect(page).to have_css(".col-md-3.has-feedback>label", visible: true, text: course_label)
+  end
 end
 
 
 Then(/^I see that the assessments are displayed which belong to the selected "([^"]*)"$/) do |course_name|
-expect(page.all(".col-md-offset-3.col-md-3 h4")[0].text).to eq(course_name)
+  expect(page).to have_css("#assessment-title-header", visible:true, text: 'Assessment Outcome')
+  expect((find_all(".col-md-offset-3.col-md-3 h4")[0]).text).to eq(course_name)
 end
 
 Then(/^I see assessments matching with that course filter are displayed$/) do
-expect(page).to have_css(".dors-well-other", visible:true,minimum:1)
+  expect(page).to have_css(".dors-well-other", visible: true, minimum: 1)
 end
 
 Then(/^I see that the assessments are displayed which belong to the selected "([^"]*)","([^"]*)" and "([^"]*)"$/) do |course1, course2, course3|
-  expect(page.all(".col-md-offset-3.col-md-3 h4")[0].text).to eq(course1)
-  expect(page.all(".col-md-offset-3.col-md-3 h4")[1].text).to eq(course3)
+  expect(page).to have_css("#assessment-title-header", visible:true, text: 'Assessment Outcome')
+  expect((find_all(".col-md-offset-3.col-md-3 h4")[0]).text).to eq(course1)
+  expect((find_all(".col-md-offset-3.col-md-3 h4")[1]).text).to eq(course3)
+end
+
+Then(/^I see the message "([^"]*)" on my assessments page$/) do |message|
+  expect(page).to have_css(".alert.alert-info", visible: true, text: message)
 end
